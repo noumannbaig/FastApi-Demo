329be918b96178b89e8ca5f6f09cf7eea654b1be
nouman baig <91089058+noumannbaig@users.noreply.github.com>
2023-08-05 22:26:35 +0500

    Update config.py

--- app/config.py
+++ app/config.py
@@ -12,7 +12,7 @@ DB_DRIVER = os.environ.get("DB_DRIVER", "postgresql+psycopg2")
 DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_PORT = os.environ.get("DB_PORT", "5432")
 DB_USER = os.environ.get("DB_USER", "postgres")
-DB_PASS = os.environ.get("DB_PASS", "Nomanbaig7051")
+DB_PASS = os.environ.get("DB_PASS", "password")
 DB_NAME = os.environ.get("DB_NAME", "User")
 # This is a postgresql specific paramter, https://www.postgresql.org/docs/9.1/libpq-ssl.html
 DB_SSLMODE = os.environ.get("DB_SSLMODE", "disable")
